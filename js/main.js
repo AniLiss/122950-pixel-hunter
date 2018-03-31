3a97ba71f2b44f64550088347129d53514dd2ba9
aniliss <anatskayalisa@gmail.com>
2018-03-31 16:07:55 +0300

    name fixing

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
 let templateIndex = 0;
-const templates = [...document.querySelectorAll(`template`)].map((template) => template.id);
+const templateList = [...document.querySelectorAll(`template`)].map((template) => template.id);
 const containerNode = document.getElementsByClassName(`central`);
 
 
 function showTemplateByNum(templateNum) {
-  const tmpl = document.getElementById(templates[templateNum]);
+  const template = document.getElementById(templateList[templateNum]);
   [...containerNode[0].childNodes].map((i) => i.remove());
-  containerNode[0].appendChild(tmpl.content.cloneNode(true));
+  containerNode[0].appendChild(template.content.cloneNode(true));
   templateIndex = templateNum;
 }
 
@@ -14,10 +14,9 @@ function showTemplateByNum(templateNum) {
 document.addEventListener(`keydown`, function (event) {
   if ((event.which === 37 && event.altKey) && templateIndex !== 0) {
     showTemplateByNum(--templateIndex);
-  } else if ((event.which === 39 && event.altKey) && templateIndex !== templates.length - 1) {
+  } else if ((event.which === 39 && event.altKey) && templateIndex !== templateList.length - 1) {
     showTemplateByNum(++templateIndex);
   }
 });
-
 
 showTemplateByNum(templateIndex);
